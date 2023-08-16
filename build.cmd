e04e712acb3dd8ae49f2916dc391e4150d334f0d
Tyler K <tyly04@gmail.com>
2023-08-15 17:03:30 -0700

    Better build.cmd tools

--- build.cmd
+++ build.cmd
@@ -7,6 +7,19 @@ for /f ^"usebackq^ eol^=^
     )
 )
 
-REM You have to edit tags in the editing kit directory, so we copy those over to here.
-xcopy /s /y "%h3ek%\tags\*" .\tags\
+REM You have to edit tags in the editing kit directory, so we copy from those tags to here.
+
+echo -----------------
+echo Copying from tags...
+echo -----------------
+
+for %%a in (%tag_directories%) do (
+    xcopy /s /y "%h3ek%\tags\%%a\*" ".\tags\%%a\"
+)
+
+echo -----------------
+echo Copying to data...
+echo -----------------
+
+REM conversely, we move our data over to the editing kit directory.
 xcopy /s /y .\data\* "%h3ek%\data\"
