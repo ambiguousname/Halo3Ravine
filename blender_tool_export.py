e8a349eeb08eb5a7841e47933eff5fdd5585aa8c
Tyler K <tyly04@gmail.com>
2025-03-25 09:21:23 -0700

    Start on helper for exports

--- blender_tool_export.py
+++ blender_tool_export.py
@@ -12,62 +12,83 @@ bl_info = {
 
 kwargs = {}
 root_dir = ""
-struct_name = ""
-name = ""
 tool_path = ""
-
-def export_ass(fbx_dir):
-    bpy.ops.export_scene.fbx(filepath=fbx_dir + name + ".fbx", global_scale=0.3, axis_forward='-Z', axis_up='Y', use_active_collection=True, use_mesh_modifiers=True)
-    fbx_from = os.path.realpath(f"{fbx_dir}\\{name}.fbx")
-    ass_to = os.path.realpath(f"{fbx_dir}\\{name}.ass")
-    subprocess.run([tool_path, 'fbx-to-ass', fbx_from, ass_to])
-
-def build_structure_from_blend():
-    fbx_dir = f"{root_dir}\\{kwargs['blender_directory']}"
-    export_ass(fbx_dir)
-    subprocess.run(["xcopy", "/y", "/f", f"{fbx_dir}\\{name}.ass", f"{kwargs['h3ek']}\\{kwargs['blender_directory']}"])
-    os.chdir(kwargs['h3ek'])
-    struct_directory = os.path.normpath(kwargs['tag_directories'])
-    subprocess.run(["tool_fast.exe", "structure-seams", struct_directory])
-    subprocess.run(["tool_fast.exe", "structure", struct_directory + "\\structure\\" + name + ".ass"])
-    os.chdir(root_dir)
-    subprocess.run("build.cmd")
-    
-    return {'FINISHED'}
+struct_name = ""
+struct_directory = ""
     
 class BlendToStructureExporter(Operator):
     bl_idname = "blend_structure_tag.export"
-    bl_label = "Export Structure"
+    bl_label = "Export Active Collection As .structure"
+
+    def export_ass(self, fbx_dir):
+        bpy.ops.export_scene.fbx(filepath=f"{fbx_dir}{self.name}.fbx", global_scale=0.3, axis_forward='-Z', axis_up='Y', use_active_collection=True, use_mesh_modifiers=True)
+        fbx_from = os.path.realpath(f"{fbx_dir}\\{self.name}.fbx")
+        ass_to = os.path.realpath(f"{fbx_dir}\\{self.name}.ass")
+        subprocess.run([tool_path, 'fbx-to-ass', fbx_from, ass_to])
+
+    def build_structure(self):
+        fbx_dir = f"{root_dir}\\{kwargs['blender_directory']}"
+        self.export_ass(fbx_dir)
+        subprocess.run(["xcopy", "/y", "/f", f"{fbx_dir}\\{self.name}.ass", f"{kwargs['h3ek']}\\{kwargs['blender_directory']}"])
+        os.chdir(kwargs['h3ek'])
+        subprocess.run(["tool_fast.exe", "structure", struct_directory + "\\structure\\" + self.name + ".ass"])
+        os.chdir(root_dir)
+        subprocess.run("build.cmd")
+        return {'FINISHED'}
     
     def execute(self, context):
-        global root_dir
-        global kwargs
-        global name
-        global struct_name
-        global tool_path
-        root_dir = bpy.path.abspath("//")
-        c = open(root_dir + "\\config.ini", "r")
-        lines = c.readlines()
-        for line in lines:
-            line = line.split("=")
-            var_name = line[0]
-            kwargs[var_name] = line[1].replace("\n", "")
-        
-        struct_name = bpy.path.basename(bpy.context.blend_data.filepath).replace(".blend", "")
-        name = struct_name + "_" + bpy.context.view_layer.active_layer_collection.name
-        tool_path = os.path.realpath(f"{kwargs['h3ek']}\\tool_fast.exe")
-        return build_structure_from_blend()
-    
+        self.name = struct_name + "_" + bpy.context.view_layer.active_layer_collection.name
+        return self.build_structure()
+
+class ExportSeams(Operator):
+    bl_idname = "blend_seam_structure.export"
+    bl_label = "Generate Structure Seams"
+
+    def execute(self, context):
+        os.chdir(kwargs['h3ek'])
+        subprocess.run(["tool_fast.exe", "structure-seams", struct_directory])
+        return {'FINISHED'}
+
+class HaloHelper(bpy.types.Panel):
+    bl_idname = "UI_PT_halo_helper"
+    bl_label = "Halo Helper"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Halo Helper"
+
+    def draw(self, context):
+        layout = self.layout
+
+        box = layout.box()
+        box.label(text="Export Tags")
+        box.operator("blend_structure_tag.export")
+        box.operator("blend_seam_structure.export")
+
 def blend_structure_export(self, context):
     self.layout.operator(BlendToStructureExporter.bl_idname, text=".structure tag from collection")
 
 def register():
     bpy.utils.register_class(BlendToStructureExporter)
-    bpy.types.TOPBAR_MT_file_export.append(blend_structure_export)
+    bpy.utils.register_class(ExportSeams)
+    bpy.utils.register_class(HaloHelper)
     
 def unregister():
     bpy.utils.unregister_class(BlendToStructureExporter)
-    bpy.types.TOPBAR_MT_file_export.remove(blend_structure_export)
+    bpy.utils.unregister_class(ExportSeams)
+    bpy.utils.unregister_class(HaloHelper)
 
 if __name__ == "__main__":
+    root_dir = bpy.path.abspath("//")
+
+    c = open(root_dir + "\\config.ini", "r")
+    lines = c.readlines()
+    for line in lines:
+        line = line.split("=")
+        var_name = line[0]
+        kwargs[var_name] = line[1].replace("\n", "")
+    tool_path = os.path.realpath(f"{kwargs['h3ek']}\\tool_fast.exe")
+    
+    struct_directory = os.path.normpath(kwargs['tag_directories'])
+    struct_name = bpy.path.basename(bpy.context.blend_data.filepath).replace(".blend", "")
+    
     register()
